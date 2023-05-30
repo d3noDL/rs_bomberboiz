57fe0be45f95ab4d525ef319c1b18fba24c29dcc
d3noDL <dino@adenogame.com>
2023-05-30 21:23:09 +0200

    MapGen
    - Fixed map generation

--- src/arena.rs
+++ src/arena.rs
@@ -1,39 +1,57 @@
 pub mod arena {
-
     use bmp::*;
-
     use colored::Colorize;
     use nalgebra::*;
 
-    pub fn build_arena() -> Matrix<String, Dyn, Dyn, VecStorage<String, Dyn, Dyn>> {
-        let map = bmp::open("img/0.bmp").unwrap();
-        let mut matrix = DMatrix::from_element(15, 15, " ".to_string());
+    pub struct Arena {
+        pub matrix: Matrix<String, Dynamic, Dynamic, VecStorage<String, Dynamic, Dynamic>>,
+    }
 
-        for (x, y) in map.coordinates() {
-            if map.get_pixel(x, y) == Pixel::new(0, 0, 0) {
-                let x: usize = usize::try_from(x).unwrap();
-                let y: usize = usize::try_from(y).unwrap();
-                matrix[(x, y)] = "▢".to_string();
-            }
-            if map.get_pixel(x, y) == Pixel::new(255, 255, 255) {
-                if rand::random() {
+    pub trait Init {
+        fn init(&self);
+    }
+
+    pub trait Update {
+        fn update(&self);
+    }
+
+    pub trait Build {
+        fn build(&mut self);
+    }
+
+    impl Build for Arena {
+        fn build(&mut self) {
+            let map = bmp::open("img/0.bmp").unwrap();
+            self.matrix = DMatrix::from_element(15, 15, " ".to_string());
+
+            for (x, y) in map.coordinates() {
+                if map.get_pixel(x, y) == Pixel::new(0, 0, 0) {
                     let x: usize = usize::try_from(x).unwrap();
                     let y: usize = usize::try_from(y).unwrap();
-                    matrix[(x, y)] = "⊞".truecolor(255, 125, 0).to_string();
+                    self.matrix[(x, y)] = "▢".to_string();
+                }
+                if map.get_pixel(x, y) == Pixel::new(255, 255, 255) {
+                    if rand::random() {
+                        let x: usize = usize::try_from(x).unwrap();
+                        let y: usize = usize::try_from(y).unwrap();
+                        self.matrix[(x, y)] = "⊞".truecolor(255, 125, 0).to_string();
+                    }
                 }
             }
         }
-        return matrix;
     }
-    pub fn update_arena(arena: Matrix<String, Dyn, Dyn, VecStorage<String, Dyn, Dyn>>) {
-        let mut pos = 0;
-        for i in arena.iter() {
-            if pos < 15 - 1 {
-                print!("{} ", i);
-                pos += 1;
-            } else {
-                pos = 0;
-                println!("{} ", i);
+
+    impl Update for Arena {
+        fn update(&self) {
+            let mut pos = 0;
+            for i in self.matrix.iter() {
+                if pos < 15 - 1 {
+                    print!("{} ", i);
+                    pos += 1;
+                } else {
+                    pos = 0;
+                    println!("{} ", i);
+                }
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,37 @@
 extern crate bmp;
+extern crate nalgebra;
 extern crate text_io;
 
-use rs_test::arena::*;
+use nalgebra::*;
+use rs_test::arena::arena::{Arena, Build, Update};
 use rs_test::player::player::Player;
 use text_io::*;
 
 fn main() {
     clear();
-    let mut players: Vec<Player> = Vec::new();
-    clear();
-    print!("Enter the number of players: ");
-    let player_count: usize = read!();
-    clear();
-    for i in 0..player_count {
-        print!("Enter the name for Player {}: ", i + 1);
-        let p_name: String = read!();
-        clear();
+    // let mut players: Vec<Player> = Vec::new();
+    // clear();
+    // print!("Enter the number of players: ");
+    // let player_count: usize = read!();
+    // clear();
+    // for i in 0..player_count {
+    //     print!("Enter the name for Player {}: ", i + 1);
+    //     let p_name: String = read!();
+    //     clear();
 
-        players.push(Player {
-            name: p_name,
-            position_x: 0,
-            position_y: 0,
-        })
-    }
+    //     players.push(Player {
+    //         name: p_name,
+    //         position_x: 0,
+    //         position_y: 0,
+    //     })
+    // }
     clear();
-    let arena_matrix = arena::build_arena();
-
-    'update: loop {
-        break 'update;
-    }
-
-    'draw: loop {
-        arena::update_arena(arena_matrix);
-        break 'draw;
-    }
+    let arena_matrix = DMatrix::from_element(0, 0, "elem".to_string());
+    let mut arena: Arena = Arena {
+        matrix: arena_matrix,
+    };
+    arena.build();
+    arena.update();
 }
 
 fn clear() {
