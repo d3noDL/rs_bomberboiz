73f546afb2c6f7e88032e9a8faff18e2e9f4c19a
d3noDL <dino@adenogame.com>
2023-05-31 15:04:02 +0200

    Messin about

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ extern crate nalgebra;
 extern crate text_io;
 
 use colored::Colorize;
-use device_query::DeviceQuery;
-use device_query::DeviceState;
-use device_query::Keycode;
 use nalgebra::*;
 use rs_test::arena::arena::Arena;
 use rs_test::player::player::Player;
+use std::io::*;
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
 //use text_io::*;
 
 fn main() {
@@ -19,7 +20,7 @@ fn main() {
     let mut arena: Arena = Arena {
         matrix: arena_matrix,
     };
-    let player: Player = Player {
+    let mut player: Player = Player {
         name: player_name,
         position_x: 1,
         position_y: 1,
@@ -29,14 +30,15 @@ fn main() {
     arena.matrix[(player.position_x, player.position_y)] = "o".green().to_string();
     arena.update();
 
-    let device_state = DeviceState::new();
-    'update: loop {
-        let keys: Vec<Keycode> = device_state.get_keys();
-        for key in keys.iter() {
-            if key == &Keycode::Escape {
-                println!("hi");
-                break 'update;
-            }
+    let input = stdin();
+
+    for c in input.keys() {
+        //i reckon this speaks for itself
+        match c.unwrap() {
+            Key::Ctrl('h') => println!("Hello world!"),
+            Key::Ctrl('q') => break,
+            Key::Alt('t') => println!("termion is cool"),
+            _ => (),
         }
     }
 }
